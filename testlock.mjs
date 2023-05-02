380232b94432b60e49d31d0cea9fd1ae04be23b3
Vjacheslav Borisov <slavb@iconicompany.com>
2023-05-02 10:17:11 +0400

    add check work

--- testlock.mjs
+++ testlock.mjs
@@ -1,28 +1,41 @@
 import lockfile from "proper-lockfile";
 import Timeout from "await-timeout";
-import createDebug from 'debug';
-import fs from 'fs';
-const debug = createDebug('testlock');
+import createDebug from "debug";
+import fs from "fs";
+const debug = createDebug("testlock");
 
 async function main() {
   const filePath = "/tmp/test";
-  fs.closeSync(fs.openSync(filePath, 'w'))
+  const resultPath = "/tmp/result";
+  fs.closeSync(fs.openSync(filePath, "w"));
 
-  let relaese = () => debug('empty release');
+  let relaese = () => debug("empty release");
   try {
-    debug('try lock');
+    debug("try lock");
     relaese = await lockfile.lock(filePath);
-    debug('start work');
-    await Timeout.set(5 * 1000);
-    debug('end work');
+    debug("check work");
+    // тут обычно читаем состояние из БД, файл для теста
+    if (!fs.existsSync(resultPath)) {
+      fs.closeSync(fs.openSync(resultPath, "w"));
+    }
+    const data = fs.readFileSync(resultPath).toString();
+    debug('data', data);
+    if (data !== "DONE") {
+      debug("start work");
+      await Timeout.set(5 * 1000);
+      fs.writeFileSync(resultPath, "DONE");
+      debug("end work");
+    } else {
+      debug("work already done");
+    }
   } catch (e) {
     // тут можно сделать проверку:
     // если e.code==='ELOCKED', вывести пользователю понятное сообщение об ошибке
-    debug('catch', 'code', e.code, 'exception', e);
+    debug("catch", "code", e.code, "exception", e);
   } finally {
-    debug('start release');
+    debug("start release");
     await relaese();
-    debug('end release');
+    debug("end release");
   }
 }
 
